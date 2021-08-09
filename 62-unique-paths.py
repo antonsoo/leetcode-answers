07c0553ba23e368d06aa84513e00afbba1002621
Anton <42787049+antonsoo@users.noreply.github.com>
2021-08-10 07:15:10 +0900

    Update 62-unique-paths.py

--- 62-unique-paths.py
+++ 62-unique-paths.py
@@ -1,8 +1,18 @@
 class Solution:
-     def uniquePaths(self, m: int, n: int) -> int:
+     def uniquePaths2(self, m: int, n: int) -> int:
         # O(nm) time, O(nm) space
         paths = [[1] * n for _ in range(m)] # make m rows of n  columns
-        for col in range(1, m):
-            for row in range(1, n):
-                paths[col][row] = paths[col - 1][row] + paths[col][row - 1]
+        for row in range(1, m):
+            for col in range(1, n):
+                paths[row][col] = paths[row - 1][col] + paths[row][col - 1]
         return paths[-1][-1]
+    
+     def uniquePaths(self, m, n):
+        # O(mn) time, O(n) space 
+        if not m or not n:
+            return 0
+        current = [1] * n
+        for row in range(1, m):
+            for col in range(1, n):
+                current[col] += current[col-1]
+        return current[-1]
