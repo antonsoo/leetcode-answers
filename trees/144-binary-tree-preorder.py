1912492c1f33eb584bea5b54213c0b3716e99cf9
Anton <42787049+antonsoo@users.noreply.github.com>
2022-03-29 08:11:04 +0000

    Create 144-binary-tree-preorder.py

---
+++ trees/144-binary-tree-preorder.py
@@ -0,0 +1,19 @@
+""" 144 - Binary Tree Preorder Traversal - Easy - 2022Q1: Adobe
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        output = []
+        def traverse(root):
+            if not root:
+                return
+            output.append(root.val)
+            traverse(root.left)
+            traverse(root.right)
+        traverse(root)
+        return output
