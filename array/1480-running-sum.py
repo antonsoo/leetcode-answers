c7b7fc68ff79970971a2b002c179d9f91d2be8f7
Tony <42787049+antonsoo@users.noreply.github.com>
2022-07-27 23:29:58 +0000

    Update 1480-running-sum.py

--- array/1480-running-sum.py
+++ array/1480-running-sum.py
@@ -1,8 +1,11 @@
 # 1480 - Running Sum of 1d Array - Easy - Amzn/Adobe/Appl
 # Problem: return the array like: runningSum[i] = sum(nums[0]…nums[i]).
+# Complexity: O(n) time, O(1) space (if we dont count the output array)
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         output = [nums[0]]
         for i in range(1, len(nums)):
             output.append(nums[i] + output[i - 1])
         return output
+    
+# Note: can also do nums[i] += nums[i-1] if we want to overwrite the original array
