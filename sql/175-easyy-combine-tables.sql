d51d3ac57b77c219fac1e862c64ac6a3b33ad446
Tony <42787049+antonsoo@users.noreply.github.com>
2022-08-18 07:48:13 +0000

    Update 175-easyy-combine-tables.sql

--- sql/175-easyy-combine-tables.sql
+++ sql/175-easyy-combine-tables.sql
@@ -1,6 +1,6 @@
 # 175. Combine Two Tables - Easy - Companies:Adobe/Amzn
- # left join will put in city, state as null when firstName, lastName are not present for
-    #those entries in city or state
+ # left join will put in city, state as null when firstName, lastName are not present for those entries in city or state
+ # that's what the question asked us to do in the descrp. btw
 SELECT firstName, lastName, city, state
 FROM Person AS p LEFT JOIN Address AS a
     ON p.personID = a.personID;
