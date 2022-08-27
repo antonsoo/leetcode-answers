e69ddb2e29ecd5189491653a7f425d03288ffad1
Tony <42787049+antonsoo@users.noreply.github.com>
2022-08-27 21:12:33 +0000

    Update 175-easyy-combine-tables.sql

--- sql/175-easyy-combine-tables.sql
+++ sql/175-easyy-combine-tables.sql
@@ -2,6 +2,7 @@
  # left join will put in city, state as null when firstName, lastName are not present for those entries in city or state
  # that's what the question asked us to do in the descrp. btw
 SELECT firstName, lastName, city, state
-FROM Person AS p LEFT JOIN Address AS a
-    ON p.personID = a.personID;
+FROM Person NATURAL LEFT JOIN Address;
+#FROM Person AS p LEFT JOIN Address AS a;
+#    ON p.personID = a.personID;
     
