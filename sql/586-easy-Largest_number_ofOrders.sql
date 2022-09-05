8a290c2e5f58f3591fc8431aac8473dcf00590df
Tony <42787049+antonsoo@users.noreply.github.com>
2022-09-05 21:37:49 +0000

    Create 586-easy-Largest_number_ofOrders.sql

---
+++ sql/586-easy-Largest_number_ofOrders.sql
@@ -0,0 +1,29 @@
+# 586. Customer Placing the Largest Number of Orders - Easy - 2022:Goog
+
+SELECT customer_number FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+
+# my failed code: error for MySQL not supporting the subquery
+# SELECT customer_number FROM Orders
+# WHERE customer_number IN 
+#     (SELECT customer_number, COUNT(DISTINCT order_number)
+#         FROM Orders
+#         ORDER BY customer_number DESC
+#         LIMIT 1
+#     );
+
+
+# from LC user gavinguojia:
+# Follow up: What if more than one customer have the largest number of orders, can you find all the customer_number in this case?
+# SELECT customer_number
+# FROM orders
+# Group BY customer_number
+# HAVING  count(order_number) = 
+# (SELECT max(numOfOrder)
+# FROM
+#     (SELECT customer_number,count(order_number) as numOfOrder
+#     FROM orders
+#     Group By customer_number) as temp)
