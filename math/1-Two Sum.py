53ec22cab65da0d57138f1f477baf81e7262fbb2
Anton Soloviev <42787049+antonsoo@users.noreply.github.com>
2024-04-22 06:33:08 -0400

    Update 1-Two Sum.py

--- math/1-Two Sum.py
+++ math/1-Two Sum.py
@@ -1,3 +1,36 @@
+# New solutions (2024, April):
+class Solution:
+    # Solution 1: An optimized solution
+    # Complexity: O(n) time, O(n) space.
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 2:
+            return [0, 1]
+        differences_dict = {} # {difference_value i.e. the corresponding_num   :  index_value of that num}
+        for ind, num in enumerate(nums):
+            diff = target - num
+            if diff in differences_dict:
+                # so if the difference is already in the dictionary of differences, 
+                # then return the index of the indeces of the two nums that add up to the target number
+                return [ind, differences_dict[diff]]
+            else:
+                differences_dict[num] = ind
+        return [] # in the case that nothing was found
+
+
+    # Solution 2: a simple, unoptimized, solution:
+    # Complexity: O(n^2) time, O(1) space.
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     for i in range(len(nums)):
+    #         x = abs(target - nums[i])
+    #         for j in range(i + 1, len(nums)):
+    #             if x == nums[j]:
+    #                 return [i, j]
+
+
+
+
+
+# Old solutions (2022 and earlier):
 class Solution:
     # assuming it exists
     def twoSum(self, nums: List[int], target: int) -> List[int]:
