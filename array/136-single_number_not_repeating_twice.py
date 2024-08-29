eec1fe64d30808263d63b0c234db391ed61c9c6d
Anton Soloviev <42787049+antonsoo@users.noreply.github.com>
2024-08-29 18:07:05 -0400

    Update 136-single_number_not_repeating_twice.py
    Additional solutions and explanations

--- array/136-single_number_not_repeating_twice.py
+++ array/136-single_number_not_repeating_twice.py
@@ -1,3 +1,4 @@
+#Old:
 """
 - problem: find the only number that isn't repeated in the array. Every other element is repeated *exactly twice.*
 - solution: hash map, sorting, brute force, math. Best: bit manipulation: a xor 0 = a. a xor a = 0. a xor b xor a = b
@@ -9,3 +10,31 @@ class Solution:
         for i in nums:
             a ^= i
         return a
+
+###############################
+
+# New: 2024-Aug-29:
+class Solution:
+    # def singleNumber(self, nums: List[int]) -> int:
+    #     nums_dict = {}
+    #     for num in nums:
+    #         if num not in nums_dict:
+    #             nums_dict[num] = 1
+    #         else: 
+    #             nums_dict[num] = 2 
+        
+    #     for key, val in nums_dict.items():
+    #         if val == 1:
+    #             return key
+
+    ## genius solution from sunakshi132: 
+    # def singleNumber(self, nums: List[int]) -> int:
+    #   return 2 * sum(set(nums)) - sum(nums))
+
+    ## genius solution 3 from satyamsinha93 and explained by umetaloper: 
+    ## 1 xor 2 xor 3 xor 1 xor 2 xor 3 xor 4 = (1 xor 1) xor (2 xor 2) xor (3 xor 3) xor 4 (commutative & associative) = 0 xor 0 xor 0 xor 4 = 4
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        return xor
