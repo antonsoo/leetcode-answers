8683c25687953e0c21f8c7b6492da93a50aa1e78
Anton Soloviev <42787049+antonsoo@users.noreply.github.com>
2025-09-18 02:43:27 -0400

    Update 2-Add_Two_Numbers.py

--- linkedLists/2-Add_Two_Numbers.py
+++ linkedLists/2-Add_Two_Numbers.py
@@ -1,11 +1,13 @@
+# Date: Sep 13-18, 2025
+# Problem number: `2. Add Two Numbers`
+# Solution credit: https://leetcode.com/problems/add-two-numbers/solutions/3675747/beats-100-c-java-python-beginner-friendly
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 
-# Problem number: `2. Add Two Numbers`
-# Solution credit: https://leetcode.com/problems/add-two-numbers/solutions/3675747/beats-100-c-java-python-beginner-friendly
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummyHead = ListNode(0)
