0853356f70dc178ab6e08161791922d07888c12d
Anton Soloviev <42787049+antonsoo@users.noreply.github.com>
2025-09-20 02:26:19 -0400

    Update 7-reverse-int.py

--- math/7-reverse-int.py
+++ math/7-reverse-int.py
@@ -1,3 +1,30 @@
+# Date(s) worked on & submitted: Sep 19-20, 2025
+# LC # 7: "Reverse Integer"
+# Problem link: https://leetcode.com/problems/reverse-integer/
+# Solution link: https://leetcode.com/problems/reverse-integer/solutions/5428589/video-using-remainder
+class Solution:
+    # One of many many solutitons:
+    def reverse(self, x: int) -> int:
+        is_negative = False
+
+        if x < 0:
+            is_negative = True
+            x *= -1
+        
+        res = 0
+        while x > 0:
+            res = (res * 10) + (x % 10)
+            x //= 10
+        
+        if res > 2 ** 31 - 1:
+            return 0
+        
+        return res * -1 if is_negative else res
+                
+
+########################################################################
+#########################################################################
+# Very old submission:
 # 7. Easy (noted as Medium on leetcode)
 # O(1) time and space because we know n will always be a small constant 
 # one of the runtimes I got is 99% faster than all Python solutions
