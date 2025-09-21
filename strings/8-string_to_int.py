64e54804719cf66ce2e685d247823f7fa79f7c79
Anton Soloviev <42787049+antonsoo@users.noreply.github.com>
2025-09-21 01:36:41 -0400

    Update 8-string_to_int.py

--- strings/8-string_to_int.py
+++ strings/8-string_to_int.py
@@ -1,3 +1,56 @@
+# Date sumbitted: Sep 20-21, 2025
+# LC Problem Number 8: "String to Integer (atoi)"
+# Problem Link: https://leetcode.com/problems/string-to-integer-atoi/
+# Solution Credit/Link: Spencer Woo -- https://leetcode.com/problems/string-to-integer-atoi/solutions/798380/fast-and-simpler-dfa-approach-python-3
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        value, state, pos, sign = 0, 0, 0, 1
+
+        if len(str) == 0:
+            return 0
+
+        while pos < len(str):
+            current_char = str[pos]
+            if state == 0:
+                if current_char == " ":
+                    state = 0
+                elif current_char == "+" or current_char == "-":
+                    state = 1
+                    sign = 1 if current_char == "+" else -1
+                elif current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    return 0
+            elif state == 1:
+                if current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    return 0
+            elif state == 2:
+                if current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    break
+            else:
+                return 0
+            pos += 1
+
+        value = sign * value
+        value = min(value, 2 ** 31 - 1)
+        value = max(-(2 ** 31), value)
+
+        return value
+
+
+
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###################################### Very old:
+
 """ 8. String to Integer. "Medium" (I think it's very easy). Facebook>>Microsft>Amazon>Google>etc
 - problem: convert a string to myatoi according to the 6 rules given.
 """
