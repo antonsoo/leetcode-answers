5f3e7668e51d8d8374c8eea1176a545a3f5872fb
Anton Soloviev <42787049+antonsoo@users.noreply.github.com>
2025-09-26 23:27:58 -0400

    Create LC-15_3Sum.py

---
+++ LC-15_3Sum.py
@@ -0,0 +1,32 @@
+# Date: Sep 26, 2025
+# LC # 15. "3Sum"
+# Problem Link: https://leetcode.com/problems/3sum/
+# Solution Credit: LC user nick-named "niits"
+# Solution link: https://leetcode.com/problems/3sum/solutions/5055810/video-two-pointer-solution
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+
+                    while nums[j] == nums[j-1] and j < k:
+                        j += 1
+        
+        return res
