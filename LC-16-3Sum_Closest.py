e198a14b4f46412f998b2872998f9c86bebfc5ee
Anton Soloviev <42787049+antonsoo@users.noreply.github.com>
2025-09-27 23:30:58 -0400

    Create LC-16-3Sum_Closest.py

---
+++ LC-16-3Sum_Closest.py
@@ -0,0 +1,41 @@
+# Date: Sep 27, 2025
+# LC # 16. "3Sum Closest"
+# Problem Link: https://leetcode.com/problems/3sum-closest/
+# Solution Credit: LC user nick-named "OvsovNikita"
+# Solution link: https://leetcode.com/problems/3sum-closest/solutions/778177/python3-runtime-52-ms-faster-than-99-77
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        return self.KSumClosest(nums, 3, target)
+
+    def KSumClosest(self, nums: List[int], k: int, target: int) -> int:
+        N = len(nums)
+        if N == k:
+            return sum(nums[:k])
+
+        # target too small
+        current = sum(nums[:k])
+        if current >= target:
+            return current
+
+        # target too big
+        current = sum(nums[-k:])
+        if current <= target:
+            return current
+        
+        if k == 1:
+            return min([(x, abs(target - x)) for x in nums], key = lambda x: x[1])[0]
+
+        closest = sum(nums[:k])
+        for i, x in enumerate(nums[:-k+1]):
+            if i>0 and x == nums[i-1]:
+                continue
+            current = self.KSumClosest(nums[i+1:], k-1, target - x) + x
+            if abs(target - current) < abs(target - closest):
+                if current == target:
+                    return target
+                else:
+                    closest = current
+
+        return closest
+        
