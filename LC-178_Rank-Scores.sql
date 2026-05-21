4f0c0b71294e45de42fa25a7462e8273535905fb
ANTON SOLOVIEV <antonnsoloviev@gmail.com>
2026-05-21 00:04:17 -0600

    Update LC-178_Rank-Scores.sql

--- LC-178_Rank-Scores.sql
+++ LC-178_Rank-Scores.sql
@@ -1,8 +1,8 @@
 # Problem: 178. Rank Scores
 # Solution: https://leetcode.com/problems/rank-scores/solutions/7045886/easy-solution-by-nramana97-8g2h/
 
-SELECT S.score ,COUNT(S2.SCORE) as `rank` FROM SCORES S,
-(SELECT DISTINCT SCORE FROM SCORES)  S2
-WHERE S.SCORE<=S2.SCORE 
+SELECT S.score, COUNT(S2.SCORE) as `rank` FROM SCORES S,
+    (SELECT DISTINCT SCORE FROM SCORES) S2
+WHERE S.SCORE <= S2.SCORE 
 GROUP BY S.ID 
 ORDER BY S.SCORE DESC;
